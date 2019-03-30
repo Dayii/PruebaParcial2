0ef42d999bc1e0440e16325accb17daf7a90eac1
dianateranargandona@gmail.com <dianateranargandona@gmail.com>
2019-03-30 12:58:36 -0400

    Commit con modelo web api

---
+++ parcial/ApiParcial/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiParcial.Models
+{
+    public enum UniType
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+
+        public StatusType Status { get; set; }
+
+
+
+    }
+}
